30dbe441fd05cf296a267ab8cdb31cefe1e8e24f
Lily Cassap <lily.cassap@yahoo.co.uk>
2024-02-07 11:48:55 +0000

    more setup

--- trashpet/trashpet/settings.py
+++ trashpet/trashpet/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "trashpetapp.apps.TrashpetappConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -115,7 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATIC_URL = "static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
