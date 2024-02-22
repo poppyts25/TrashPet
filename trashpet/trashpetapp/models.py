6493477d43022a6d82332dac2abc0f4f377b775e
sylvimelia <ss1527@exeter.ac.uk>
2024-02-22 12:45:19 +0000

    garden html placeholder

--- trashpet/trashpetapp/models.py
+++ trashpet/trashpetapp/models.py
@@ -6,6 +6,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     leaves = models.DecimalField(max_digits=10, decimal_places=0, default=0)
     pet_name = models.CharField(max_length=30, default="Trash Pet") # set different / validate pet name length???
+    codes = models.CharField(default="")
 
     def __str__(self):
         return f"{self.user.username}"
