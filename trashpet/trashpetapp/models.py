1244416ac16cf86366c327543722cef9bbf20943
sylvimelia <ss1527@exeter.ac.uk>
2024-02-28 12:20:02 +0000

    fixing database locking

--- trashpet/trashpetapp/models.py
+++ trashpet/trashpetapp/models.py
@@ -7,7 +7,9 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     leaves = models.DecimalField(max_digits=10, decimal_places=0, default=0)
     pet_name = models.CharField(max_length=30, default="Trash Pet") # set different / validate pet name length???
-    accessories = models.CharField(max_length=200, default='{Cap":False, "Crown":False, "Socks":False, "Bottle":True }')
+    locked_list = {"Cap":False, "Crown":False, "Socks":False, "Bottle":True }
+    locked_list = json.dumps(locked_list)               
+    accessories = models.CharField(max_length=200, default=locked_list)
 
 
 
