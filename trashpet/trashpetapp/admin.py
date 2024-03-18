afa1c64aaaa9227ff748fff1a676f1b85ef7b31b
sylvimelia <ss1527@exeter.ac.uk>
2024-03-18 00:35:20 +0000

    Fixing migrations

--- trashpet/trashpetapp/admin.py
+++ trashpet/trashpetapp/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import UserProfile, Accessory, Marker
+from .models import UserProfile, Accessory, Marker, LeavesCode
 # Register your models here.
 admin.site.register(UserProfile)
 admin.site.register(Accessory)
 admin.site.register(Marker)
+admin.site.register(LeavesCode)
 
 
 # @admin.register(Marker)
