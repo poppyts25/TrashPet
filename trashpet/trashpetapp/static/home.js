d7d52b4c452b04eddeb6434675285a4e5e7af4f6
sylvimelia <ss1527@exeter.ac.uk>
2024-03-18 00:58:31 +0000

    home.js

---
+++ trashpet/trashpetapp/static/home.js
@@ -0,0 +1,40 @@
+document.getElementById("feed-button").addEventListener("click", function () {
+  alert("Yum! Thanks for feeding me.");
+});
+
+document.getElementById("play-button").addEventListener("click", function () {
+  alert("Yay! Playing is fun.");
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  applySavedItems(); // Apply the saved items to the pet on the home page
+});
+
+function applySavedItems() {
+  const items = [
+    "/static/images/socks.png",
+    "/static/images/bottle.png",
+    "/static/images/crown.png",
+    "/static/images/cap.png",
+    "/static/images/guitar.png",
+  ];
+  items.forEach((itemSrc, index) => {
+    const itemId = `item-${index}`;
+    if (localStorage.getItem(itemId)) {
+      // Item was previously added, recreate it on the pet
+      if (!document.getElementById(itemId)) {
+        // Check to avoid duplicating the item
+        const newItem = document.createElement("img");
+        newItem.src = itemSrc;
+        newItem.className = "pet-layered-item";
+        newItem.id = itemId;
+        document.getElementById("pet-container").appendChild(newItem);
+      }
+    }
+  });
+
+  const savedBackgroundImage = localStorage.getItem("petBackgroundImage");
+  if (savedBackgroundImage) {
+    document.querySelector(".pet-background").src = savedBackgroundImage;
+  }
+}
