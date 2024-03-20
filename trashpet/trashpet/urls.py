29d6ac9e0726758c1be4c79ac3f7ff6e27ce7fa1
sylvimelia <ss1527@exeter.ac.uk>
2024-03-20 18:49:54 +0000

    fixing urls

--- trashpet/trashpet/urls.py
+++ trashpet/trashpet/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
-    path("trashpetapp/", include("trashpetapp.urls")),
+    path("", include("trashpetapp.urls")),
     path("admin/", admin.site.urls),
     
 ]
